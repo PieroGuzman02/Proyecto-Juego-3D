ea274658f8bb3fb568a788d112370ec9ea306c68
Talleres Puente Alto <tallerespa@inacapmail.cl>
2022-12-05 21:15:17 -0300

    Avance2

---
+++ Assets/Scripts/CambiarPantalla.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarPantalla : MonoBehaviour
+{
+    public void Cambiar()
+    {
+    SceneManager.LoadScene("Nivel 1");
+    }
+}
+
